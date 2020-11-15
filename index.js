2fa1ce2f2d3bbeec0619a546c6bd451c092aa1b6
Marisa You <marisayou2017@u.northwestern.edu>
2020-11-15 14:50:42 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+    return drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+    return drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop();
+    return drivers;
+}
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift();
+    return drivers;
+}
+
+function appendDriver(name) {
+    return [...drivers, name];
+}
+
+function prependDriver(name) {
+    return [name, ...drivers];
+}
+
+function removeLastDriver() {
+    return drivers.slice(0, drivers.length - 1);
+}
+
+function removeFirstDriver() {
+    return drivers.slice(1);
+}
